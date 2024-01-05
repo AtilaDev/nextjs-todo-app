f7b25e0eea9312591d8d6660911b92bd70f078ab
Leandro <atiladevelop@gmail.com>
2024-01-05 18:56:31 -0300

    chore: hide Tasks title if there not tasks in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,9 @@ export default async function HomePage() {
       </div>
       <Divider className='my-5 bg-slate-200' />
       <div className='flex flex-col justify-start ml-10'>
-        <div className='text-lg font-bold mb-5'>Tasks</div>
+        {tasks.length !== 0 && (
+          <div className='text-lg font-bold mb-5'>Tasks</div>
+        )}
         <div className='px-4'>
           <div className='text-sm'>{renderedTasks}</div>
         </div>
